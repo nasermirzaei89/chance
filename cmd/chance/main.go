ff5354d13cd81c250cc9ee01135f73a09e2e7b21
Naser Mirzaei <nasermirzaei89@gmail.com>
2018-12-27 23:45:03 +0330

    add cli

--- cmd/chance/main.go
+++ cmd/chance/main.go
@@ -2,14 +2,74 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"log"
+	"os"
 
 	"github.com/nasermirzaei89/chance"
+
+	"github.com/urfave/cli"
 )
 
 func main() {
-	ch := chance.New(chance.SetSeed(time.Now().UnixNano()))
-	fmt.Println(ch.Bool())
-	fmt.Println(ch.String(chance.SetStringLength(10)))
-	fmt.Println(ch.Int(chance.SetIntMax(100), chance.SetIntMin(90)))
+	app := cli.NewApp()
+	app.Name = "chance"
+	app.Usage = "Random generator"
+
+	var seed int64
+	app.Flags = []cli.Flag{
+		cli.Int64Flag{
+			Name:        "seed",
+			Usage:       "seed for random generator",
+			Value:       1,
+			Destination: &seed,
+		},
+	}
+
+	app.Commands = []cli.Command{
+		cmdBool(seed),
+		cmdString(seed),
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func cmdBool(seed int64) cli.Command {
+	return cli.Command{
+		Name:  "bool",
+		Usage: "Generates a random bool value",
+		Action: func(c *cli.Context) error {
+			fmt.Print(chance.New(chance.SetSeed(seed)).Bool())
+			return nil
+		},
+	}
+}
+
+func cmdString(seed int64) cli.Command {
+	var length int
+	var pool string
+	return cli.Command{
+		Name:  "string",
+		Usage: "Generates a random string value",
+		Flags: []cli.Flag{
+			cli.IntFlag{
+				Name:        "length",
+				Usage:       "Sets length of random string",
+				Value:       5,
+				Destination: &length,
+			},
+			cli.StringFlag{
+				Name:        "pool",
+				Usage:       "Sets pool for random string",
+				Value:       "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()[]",
+				Destination: &pool,
+			},
+		},
+		Action: func(c *cli.Context) error {
+			fmt.Print(chance.New(chance.SetSeed(seed)).String(chance.SetStringLength(length), chance.SetStringPool(pool)))
+			return nil
+		},
+	}
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ require (
 	github.com/client9/misspell v0.3.4 // indirect
 	github.com/fzipp/gocyclo v0.0.0-20150627053110-6acd4345c835 // indirect
 	github.com/gordonklaus/ineffassign v0.0.0-20180909121442-1003c8bd00dc // indirect
+	github.com/urfave/cli v1.20.0
 	golang.org/x/tools v0.0.0-20181221235234-d00ac6d27372 // indirect
 )
